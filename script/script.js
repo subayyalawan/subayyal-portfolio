8427cbb4c6df07ab07197154a815e8e4bbb13bff
Shibly9 <subayyalawan99@gmail.com>
2023-07-23 00:38:40 +0500

    last commit

--- script/script.js
+++ script/script.js
@@ -1,15 +1,48 @@
-let navlink=document.querySelectorAll('.nav-link')
+let sections = document.querySelectorAll('section')
+let navlinks = document.querySelectorAll('.nav-link')
 
-navlink.forEach(e=>{
-    // if(e.href === window.location.href){
-    //     e.setAttribute('aria-current', 'page')
-    // }
-    console.log(e.href, window.location.href)
-})
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height){
+//             // console.log(hello)
+//             navlinks.forEach(links=>{
+//                 links.classList.remove('active');
+//                 // if (links.classList.contains(id)){
+//                 //     links.classList.add('active')
+//                 // }
+//                 // document.querySelector(`.nav-links[href*=' + id + ']`).classList.add('active')
+//             })
+//         }
+//     })
+// }
+
+window.addEventListener('scroll', () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    let top = window.scrollY;
+    let sectionHeight = section.offsetHeight;
+    if (top >= sectionTop - 400) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navlinks.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
 
 let nav = document.querySelector('nav')
 let header = document.querySelector('header')
-const offset = window.scrollY
 
 window.addEventListener('scroll', ()=>{
     
